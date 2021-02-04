35e368f04194ee66268f8d7dd882d4f864530b49
Rob Earlam <robert.earlam@gmail.com>
2021-02-04 16:29:58 +1100

    Initial Commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Layout from '../components/layout'
+
+export default function Home() {
+    return (
+        <Layout>
+            <h1>This site will have some country data!</h1>
+        </Layout>
+    )
+}
